a6567a0a2eeec2898f2c777519591ba78b31bcc7
Sounak Rong <sounakrong10@gmail.com>
2026-09-25 18:34:47 +0530

    Fix DJANGO_SETTINGS_MODULE environment variable

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "smart_waste_analytical_system.settings"
+        "DJANGO_SETTINGS_MODULE", "smart-waste-analytical-system.settings"
     )
     try:
         from django.core.management import execute_from_command_line
